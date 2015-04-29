069b82d9ce59378a6159b33de3d28c75d8740ae4
Ravindranathan, Pradeepan <pravindranathan@paypal.com>
2015-04-28 23:46:13 -0700

    About Page title change

--- routes/arryoutube.js
+++ routes/arryoutube.js
@@ -116,6 +116,6 @@ exports.displaybargraph = function(req, res) {
 
 exports.welcome = function(req, res) {
 	console.log('User-Agent: ' + req.headers['user-agent']);
-	res.render('about', { title: 'About' });
+	res.render('about', { title: 'About ARR YouTube Project' });
 };
 
